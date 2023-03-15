896d826dae0f719ef0307016e79fac849aa7fc40
badanaut <49814350+badanaut@users.noreply.github.com>
2023-03-15 23:52:50 +0200

    Add addTodoItem function

--- src/js_modules/userInterface.js
+++ src/js_modules/userInterface.js
@@ -1,5 +1,13 @@
+import createTodoItem from './createTodoItem';
+
 export default class userInterface {
   static loadHomePage() {
 
+  }
+
+  static addTodoItem(itemName, description, priority) {
+    const todoList = [];
+    const newTodoItem = createTodoItem(itemName, description, priority);
+    todoList.push(newTodoItem);
   }
 }
