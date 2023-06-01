71977009e1a1aec579ab4cb6c45ea44c19ecb229
badanaut <49814350+badanaut@users.noreply.github.com>
2023-06-01 14:01:52 +0300

    Fix user inteface issue

--- src/js_modules/userInterface.js
+++ src/js_modules/userInterface.js
@@ -1,15 +1,5 @@
-import createTodoItem from "./createTodoItem";
+const userInterface = {
+  loadHomePage: () => console.log("hello!"),
+};
 
-export default class userInterface {
-  static loadHomePage() {
-    const taskContainer = document.getElementById("task-overview");
-    taskContainer.innerHTML = `<div id="task-category">Inbox</div>
-              <div id="to-do-container"></div>`;
-  }
-
-  static addTodoItem(itemName, description, priority) {
-    const todoList = []; // to add code that get to do items from html page
-    const newTodoItem = createTodoItem(itemName, description, priority);
-    todoList.push(newTodoItem);
-  }
-}
+export default userInterface;
